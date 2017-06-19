f2d22dea7ad92ecd24b59594bbf3db26cc0fe766
ztree <hunter.z@263.net>
2017-06-19 11:55:39 +0800

    完善 markdown 对于 删除线处理的功能
    例如：
    
    "
    ~~删除线 001~~
    ~~删除线 002~~
    
    ~~删除线 003~~~~删除线 004~~
    
    ~~删除线 005~~abc
    "

--- Markdown.Extra.js
+++ Markdown.Extra.js
@@ -891,8 +891,10 @@
 
     Markdown.Extra.prototype.strikethrough = function (text) {
         // Pretty much duplicated from _DoItalicsAndBold
-        return text.replace(/([\W_]|^)~T~T(?=\S)([^\r]*?\S[\*_]*)~T~T([\W_]|$)/g,
-            "$1<del>$2</del>$3");
+        // return text.replace(/([\W_]|^)~T~T(?=\S)([^\r]*?\S[\*_]*)~T~T([\W_]|$)/g,
+        //     "$1<del>$2</del>$3");
+        return text.replace(/~T~T(?=\S)([^\r]*?\S[\*_]*)~T~T/g,
+            "<del>$1</del>");
     };
 
 
